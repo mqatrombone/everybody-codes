f38b0e791fe013e77ed23d68abcaf08203f1b2a6
Matthew Andrews <mqatrombone@outlook.com>
2025-11-26 20:04:00 -0600

    Enable choosing left by number

--- 2025/01/tests/NameListTests.cs
+++ 2025/01/tests/NameListTests.cs
@@ -37,4 +37,35 @@ public class NameListTests
 
         Assert.That(actual.Current, Is.EqualTo(actual.List[0]));
     }
+
+    [Test]
+    [TestCase("Vyrdax,Drakzyph,Fyrryn,Elarzris", 1, "Drakzyph")]
+    [TestCase("Vyrdax,Drakzyph,Fyrryn,Elarzris", 2, "Fyrryn")]
+    [TestCase("Vyrdax,Drakzyph,Fyrryn,Elarzris", 3, "Elarzris")]
+    [TestCase("Vyrdax,Drakzyph,Fyrryn,Elarzris", 5, "Elarzris")]
+    public void RightWithArgGoesCorrectDistance(string source, int count, string expected)
+    {
+        NameList sut = new(source.Split(','));
+
+        sut.Right(count);
+
+        Assert.That(sut.Current, Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCase("Vyrdax,Drakzyph,Fyrryn,Elarzris", 1, "Fyrryn")]
+    [TestCase("Vyrdax,Drakzyph,Fyrryn,Elarzris", 2, "Drakzyph")]
+    [TestCase("Vyrdax,Drakzyph,Fyrryn,Elarzris", 3, "Vyrdax")]
+    [TestCase("Vyrdax,Drakzyph,Fyrryn,Elarzris", 5, "Vyrdax")]
+    public void LeftWithArgGoesCorrectDistance(string source, int count, string expected)
+    {
+        // Arrange
+        NameList sut = new(source.Split(','));
+
+        // Act
+        sut.Right(3);
+        sut.Left(count);
+
+        Assert.That(sut.Current, Is.EqualTo(expected));
+    }
 }
