6d10e35a6ede068cfe5d2e7e2788f4cafeb1517c
Matthew Andrews <mqatrombone@outlook.com>
2025-11-29 12:17:57 -0600

    Reduce INameList surface

--- 2025/01/src/INameList.cs
+++ 2025/01/src/INameList.cs
@@ -2,11 +2,6 @@ namespace Whispers;
 
 public interface INameList
 {
-    IReadOnlyList<string> List { get; }
-    string Current { get; }
-    void Right();
     void Right(int count);
-
-    void Left();
     void Left(int count);
 }
