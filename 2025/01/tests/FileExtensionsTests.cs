e035f76f7f5035b6a575d12206bf77a41b99bf07
Matthew Andrews <mqatrombone@outlook.com>
2025-11-28 21:24:43 -0600

    Fix whitespace

--- 2025/01/tests/FileExtensionsTests.cs
+++ 2025/01/tests/FileExtensionsTests.cs
@@ -17,14 +17,14 @@ public class FileExtensionsTests
 
         Assert.That(inputNames, Is.EqualTo(expectedNames));
     }
-    
+
     [Test]
     public async Task CommandsParseCorrectly()
     {
         var inputFileStream = await File.ReadAllLinesAsync("TestInput.txt");
 
         var inputCommands = inputFileStream.ParseCommands();
-        
+
         // R3,L2,R3,L1
         List<NameListCommand> expectedCommands =
         [
@@ -33,7 +33,7 @@ public class FileExtensionsTests
             new() { Command = NameListExtensions.Right, Count = 3 },
             new() { Command = NameListExtensions.Left, Count = 1 }
         ];
-        
+
         Assert.That(inputCommands, Is.EqualTo(expectedCommands));
     }
 }
