a062a8dfc8fa2e79b6c5213c52f5246e7c0d939f
Matthew Andrews <mqatrombone@outlook.com>
2025-11-28 22:04:34 -0600

    Complete Phase 1

--- 2025/01/src/Program.cs
+++ 2025/01/src/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Whispers;
+﻿using System.Collections.Immutable;
+
+namespace Whispers;
 
 public static class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var inputData = await File.ReadAllLinesAsync("Phase1Input.txt");
+
+        NameList names = new(inputData.ParseNames());
+        ImmutableList<NameListCommand> commands = inputData.ParseCommands();
+
+        commands.ForEach(command => command.Command(names, command.Count));
+
+        Console.WriteLine($"Name is {names.Current}");
     }
 }
